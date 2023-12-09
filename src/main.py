e3a294edbe2ad46486ca3b8e130d965529b9bff1
useongee <118250509+USEONGEE@users.noreply.github.com>
2023-12-10 02:17:29 +0900

    docs: add annotation

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ curr_directory = os.path.join(ROOT, 'src')
 resource_directory = os.path.join(ROOT, 'resource')
 file_list = os.listdir(resource_directory)
 
-# load
+# load method
 face_cascade = cv2.CascadeClassifier(os.path.join(curr_directory, 'haarcascade_frontalface_default.xml'))
 eyes_cascade = cv2.CascadeClassifier(os.path.join(curr_directory, 'haarcascade_eye.xml'))
 
@@ -26,11 +26,12 @@ for filename in file_list :
             faces = face_cascade.detectMultiScale(gray, 1.1, 12, minSize=(80, 80))
 
             for (x, y, w, h) in faces:
+                # detect face
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 face_gray = gray[y:y + h, x:x + w]
                 
-                # 눈 검출
                 eyes = eyes_cascade.detectMultiScale(face_gray)
+                # detect eyes
                 for (ex, ey, ew, eh) in eyes:
                     cv2.rectangle(frame, (x + ex, y + ey), (x + ex + ew, y + ey + eh), (0, 255, 0), 2)
             
